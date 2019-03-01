ed98f3e00a4b396f06b69b16f3baed5788701b40
asuleyman2403 <asuleyman2403@gmail.com>
2019-03-02 05:02:59 +0600

    footer

--- sales-reseller/src/app/app.module.ts
+++ sales-reseller/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageFooterComponent } from './components/page-footer/page-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageFooterComponent
   ],
   imports: [
     BrowserModule,
