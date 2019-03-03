6dba7c85e22df18c8af15e288d67dcefbfcddeb9
asuleyman2403 <asuleyman2403@gmail.com>
2019-03-04 00:18:40 +0600

    policy_block

--- sales-reseller/src/app/app.module.ts
+++ sales-reseller/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageFooterComponent } from './components/page-footer/page-footer.component';
+import { CategoryBlockComponent } from './components/category-block/category-block.component';
+import { PolicyBlockComponent } from './components/policy-block/policy-block.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageFooterComponent
+    PageFooterComponent,
+    CategoryBlockComponent,
+    PolicyBlockComponent
   ],
   imports: [
     BrowserModule,
---
+++ sales-reseller/src/app/components/category-block/category-block.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-block',
+  templateUrl: './category-block.component.html',
+  styleUrls: ['./category-block.component.css']
+})
+export class CategoryBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
