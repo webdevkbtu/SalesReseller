e47b831a375ddc8d3c42537842c17c9e0ab9aa8a
hentaai <emforimpstuff@gmail.com>
2019-04-04 13:30:31 +0600

    unfortunate js

--- sales-reseller/src/app/app.component.ts
+++ sales-reseller/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+declare var jQuery: any;
+declare var ocajaxlogin: any;
+declare var CloudZoom: any
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
